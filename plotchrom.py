8a10fd38ec0b884a1d4e274a1e45e8c8d02139be
Jayghosh Rao <j.rao@fz-juelich.de>
2020-10-20 10:20:23 +0200

    scripts: update dumpy, plotchrom.py, and vis.py

--- plotchrom.py
+++ plotchrom.py
@@ -4,13 +4,15 @@
 # Usage: ./plotChrom.py <file> <file>
 # Output: plot.pdf and plot.jpg
 
-# TODO: argparse support for xlabel, ylabel and title
-# TODO: argparse support for plt.show
-# TODO: argparse support for delimiter
-
 import matplotlib.pyplot as plt
-# import csv
 import sys
+import argparse
+
+def normalize(data):
+    return [ x/data[-1] for x in data ]
+
+def rescale(data, factor):
+    return [x*factor for x in data]
 
 def readChromatogram(data_path):
     time= []
@@ -25,6 +27,53 @@ def readChromatogram(data_path):
                 conc.append(float(data_line[1]))
     return time, conc
 
+ap = argparse.ArgumentParser()
+ap.add_argument("files", nargs='*', help="files to plot")
+ap.add_argument("-t", "--title", required=False, default="Chromatogram",
+        help="title")
+ap.add_argument("-x", "--xlabel", required=False, default="Time",
+        help="xlabel")
+ap.add_argument("-y", "--ylabel", required=False, default="Concentration",
+        help="ylabel")
+ap.add_argument("-l", "--labels", required=False, nargs='*',
+        help="legend labels")
+ap.add_argument("-ls", "--linestyles", required=False, nargs='*',
+        help="linestyles = solid dashed ...")
+ap.add_argument("-m", "--markers", required=False, nargs='*',
+        help="markers = s, o, ...")
+ap.add_argument("-xl", "--xlims", required=False,nargs=2, type=float,
+        help="x axis limits")
+ap.add_argument("-yl", "--ylims", required=False,nargs=2, type=float,
+        help="y axis limits")
+ap.add_argument("-f", "--fill", required=False, action='store_true',
+        help="fill area under curve")
+ap.add_argument("-n", "--normalize", required=False, action='store_true',
+        help="normalize y data to the last value")
+ap.add_argument("-nl", "--no-legend", required=False, action='store_true',
+        help="don't show legend")
+ap.add_argument("-o", "--output", required=False,
+        help="output file")
+ap.add_argument("-r", "--rescale", type=float, required=False,
+        help="Rescale graph with multiplicative factor (1/holdup-ratio)")
+ap.add_argument("-sr", "--save-rescaled", required=False, action='store_true',
+        help="File to save rescaled data")
+args = vars(ap.parse_args())
+
+if not args['labels']:
+    args['labels'] = args['files']
+
+if not args['linestyles']:
+    args['linestyles'] = [ 'solid' ] * len(args['files'])
+    # args['linestyles'] = []
+elif len(args['linestyles']) == 1:
+    args['linestyles'] = args['linestyles'] * len(args['files'])
+
+if not args['markers']:
+    args['markers'] = [ None ] * len(args['files'])
+    # args['markers'] = []
+elif len(args['markers']) == 1:
+    args['markers'] = args['markers'] * len(args['files'])
+
 
 with plt.style.context(['science']):
     fig, ax = plt.subplots()
@@ -32,17 +81,37 @@ with plt.style.context(['science']):
     ys = []
     lines = []
     count =0
-    for arg in sys.argv[1:]:
-        x, y = readChromatogram(arg)
-        line = ax.plot(x, y, label=arg)
+    for filename,label,linestyle,marker in zip(args['files'], args['labels'], args['linestyles'], args['markers']):
+        x, y = readChromatogram(filename)
+        if args['normalize']:
+            y = normalize(y)
+        if args['rescale']:
+            x = rescale(x, args['rescale'])
+        line = ax.plot(x, y, label=label, marker=marker, linestyle=linestyle)
+        if args['fill']:
+            ax.fill_between(x, y, 0, interpolate=True)
         xs.append(x)
         ys.append(y)
         lines.append(line)
         count+=1
-    legend = ax.legend(loc='best', shadow=True)
-    ax.set(title='Chromatogram')
-    ax.set(xlabel='Time')
-    ax.set(ylabel='Concentration')
+    if not args['no_legend']:
+        legend = ax.legend(loc='best', shadow=True)
+    ax.set(title=args['title'])
+    ax.set(xlabel=args['xlabel'])
+    ax.set(ylabel=args['ylabel'])
     ax.autoscale(tight=True)
-    fig.savefig('plot.pdf')
-    # fig.savefig('plot.jpg', dpi=300)
+    if args['xlims']:
+        plt.xlim(args['xlims'])
+    if args['ylims']:
+        plt.ylim(args['ylims'])
+    if args['output']:
+        fig.savefig(args['output'])
+    plt.show()
+
+
+    if args['save_rescaled']:
+        import csv
+        for filename,x,y in zip(args['files'], xs, ys):
+            with open(filename+'_postscaled', 'w') as f:
+                writer = csv.writer(f, delimiter=' ')
+                writer.writerows(zip(x, y))
--- vis.py
+++ vis.py
@@ -5,10 +5,10 @@ A Paraview script to aid the postprocessing of data. I wrote it mainly to genera
 
 Usage by examples:
     > ./vis.py -h
-    > pvpython --force-offscreen-rendering /path/to/vis.py **/*pvtu -s -c scalar_2 scalar_3 -p slice  ## for listed pvtu files, output scalar_2 and scalar_3 slice data
-    > mpirun -np 4 pvbatch /path/to/vis.py -f pvtu -s                   ## looks for pvtu files in current folder, clips them, snapshots all scalars,
+    > pvpython --force-offscreen-rendering /path/to/vis.py **/*pvtu -a -c scalar_2 scalar_3 -p slice  ## for listed pvtu files, output scalar_2 and scalar_3 slice data
+    > mpirun -np 4 pvbatch /path/to/vis.py -f pvtu -a                   ## looks for pvtu files in current folder, clips them, animates all scalars,
     > mpirun -np 4 pvbatch /path/to/vis.py **/*pvtu -d out-distributed  ## for listed pvtu files, apply d3 filter and write to out-distributed
-    > mpirun -np 4 pvbatch /path/to/vis.py example-case_{150..300}.pvtu -s  ## Uses bash's sequence expansion
+    > mpirun -np 4 pvbatch /path/to/vis.py example-case_{150..300}.pvtu -a  ## Uses bash's sequence expansion
 
 Usage notes:
     > don't forget to use a processing flag like -s or -d or -w
@@ -28,7 +28,7 @@ Usage notes:
 ## TODO: Parametrize background color
 ## TODO: fix timestep data (probably in mixd2pvtu)
 ## TODO: rotated views [Good for image resolution in long columns]
-## TODO: After updatescalarbars() they are always visible even with -nsb
+## DONE: After updatescalarbars() they are always visible even with -nsb
 
 import argparse
 from paraview.simple import *
@@ -39,20 +39,152 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os
 import sys
+import csv
+import pickle
+import struct
+
+# import h5py ##NOTE: Is perhaps better as a standard format, but issue with the version of paraview compiled locally at time of scripting. So working with bin files.
 
 def MIXDReader():
     pass
 
-def mass_defect(reader, **kwargs):
+def appendToBin(arr, filename, f):
+    with(open(filename, 'ab')) as output:
+        for i in arr:
+            output.write(struct.pack(f, i))
+
+def pickler(item, filename):
+    """
+    > Save item to file.
+    > For easy transport of lists/arrays to cadet
+    """
+    with open(filename, "wb") as f:
+        pickle.dump(item, f)
+        print("pickled", filename)
+
+def unpickler(filename):
+    with open(filename, "rb") as f:
+        item = pickle.load(f)
+        print("Unpickled", filename)
+    return item
+
+def csvWriter(filename, x, y):
+    with open(filename, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(zip(x, y))
+
+## TODO: Test written bin in mpi mode
+def bead_loading(reader, **kwargs):
+    colorVars = kwargs.get('colorVars', reader.PointArrayStatus) or reader.PointArrayStatus
+
+    try:
+        timeKeeper = GetTimeKeeper()
+        nts = len(reader.TimestepValues)
+    except:
+        nts = 1
+        pass
+
+    if nts == 0:
+        nts = 1
+    ncv = len(colorVars)
+
+    print("nts:", nts)
+    print("ncv:", ncv)
+
+    renderView1 = GetActiveViewOrCreate('RenderView')
+    # display = Show(reader, renderView1)
+    # Hide(reader, renderView1)
+
+    connectivity = Connectivity(Input=reader)
+    connectivityDisplay = Show(connectivity, renderView1)
+    Hide(connectivity, renderView1)
+
+    # NOTE: Threshold  range will be (0, n) where n is number of beads.
+    # Typically, the interstitial domain is the last, n+1th region.
+    # Here, we ignore the interstitial region by setting nbeads = n, and not n+1.
+    nbeads = int(connectivity.PointData.GetArray("RegionId").GetRange()[1])
+    print("Number of Objects:", nbeads)
+
+    appendToBin([nts, nbeads, ncv],'bead_loading.inf', '=i')
+    dataArr = np.zeros((nts, nbeads, ncv))
+    coordArr = np.zeros((nbeads,4))
+
+    for index in range(nbeads):
+        (xmin,xmax,ymin,ymax,zmin,zmax) = GetActiveSource().GetDataInformation().GetBounds()
+        # print("Threshold coordinate bounds:",xmin,xmax,ymin,ymax,zmin,zmax)
+        x = (xmax + xmin)/2
+        y = (ymax + ymin)/2
+        z = (zmax + zmin)/2
+        r = (xmax - xmin + ymax - ymin + zmax - zmin)/2
+        # print("xyzr:",x, y, z, r)
+        # coordArr[index,:] = np.array([x, y, z, r])
+        appendToBin([x,y,z,r],'bead_loading.xyzr', '=d')
+
+    for timestep in range(nts):
+        timeKeeper.Time = timestep
+        print("Processing timestep: ", timestep, end="\r")
+
+        for index in range(nbeads):
+            threshold = Threshold(Input=connectivity)
+            threshold.ThresholdRange = [index, index]
+            thresholdDisplay = Show(threshold, renderView1)
+            # threshold.UpdatePipeline()
+
+            integrated = IntegrateVariables(Input=threshold)
+            intdata = servermanager.Fetch(integrated)
+            intdata = dsa.WrapDataObject(intdata)
+
+            values = []
+            for colorVar in colorVars:
+                value = intdata.PointData[colorVar]
+                value = ns.vtk_to_numpy(value)
+                values.append(value[0])
+
+            # dataArr[timestep,index,:] = np.array(values)
+            # f['data'][timestep,index,:] = np.array(values)
+            appendToBin(values,'bead_loading.dat', '=d')
+
+    # ## FIXED: Append to binary inside loop instead
+    # ## Save data for further processing. Pickle?
+    # print("Pickling data...")
+    # pickler(dataArr, 'bead.dataArr.pickle')
+    # pickler(coordArr, 'bead.coordArr.pickle')
+    # print(dataArr.size, coordArr.size)
+
+def mass_flux(reader, **kwargs):
+    scalarBarVisible = kwargs.get('scalarBarVisible', True)
+    geometry = kwargs.get('geometry', [1750, 1300])
+    axisVisible = kwargs.get('axisVisible', True)
+    colorVars = kwargs.get('colorVars', reader.PointArrayStatus) or reader.PointArrayStatus
+    nSlice = kwargs.get('nSlice', 1) or 1
 
     renderView1 = GetActiveViewOrCreate('RenderView')
     display = Show(reader, renderView1)
+    # display.Representation = 'Surface With Edges'
 
     (xmin,xmax,ymin,ymax,zmin,zmax) = GetActiveSource().GetDataInformation().GetBounds()
+    print(xmin,xmax,ymin,ymax,zmin,zmax)
 
-    zarr = []
+    Hide(reader, renderView1)
 
-    for zpos in np.linspace(1.0*zmin, zmax, 100):
+    ## NOTE: Only takes takes one color
+    colorVar = colorVars[0]
+    flowrate = []
+    zs = []
+
+    count = 0
+
+    try:
+        ## Set timestep to last timestep (last file in series)
+        timeKeeper = GetTimeKeeper()
+        timeKeeper.Time = reader.TimestepValues[-1]
+    except:
+        pass
+
+    for zpos in np.linspace(zmin, zmax, nSlice):
+
+        count = count + 1
+        print("Loop: ", count, zpos)
         projection = Slice(Input=reader)
         Hide3DWidgets(proxy=projection.SliceType)
 
@@ -61,34 +193,97 @@ def mass_defect(reader, **kwargs):
         projection.SliceOffsetValues = [0.0]
 
         projection.SliceType.Origin = [0.0, 0.0, zpos]
-        projection.SliceType.Normal = [0.0, 0.0, 1.0]
+        projection.SliceType.Normal = [0.0, 0.0, -1.0]
+        projection.UpdatePipeline()
 
-        cellSize = CellSize(Input=projection)
-        cellSize.ComputeSum = 1
+        ## {{{
+        # projectionDisplay = Show(projection, renderView1)
+        # projectionDisplay.Representation = 'Surface'
+        # # projectionDisplay.Representation = 'Surface With Edges'
+        # renderView1.OrientationAxesVisibility = int(axisVisible)
+        # projectionDisplay.RescaleTransferFunctionToDataRange()
 
-        ## NOTE: POINT DATA TO CELL DATA BEFORE INTEGRATE?
-        ## TODO: Get Bounding Box
+        # renderView1.Update()
+        # renderView1.ViewSize = geometry
+        # renderView1.ResetCamera()
+
+        # ColorBy(projectionDisplay, ('POINTS', colorVar))
+        # projectionDisplay.RescaleTransferFunctionToDataRange()
+
+        # wLUT = GetColorTransferFunction(colorVar)
+        # wPWF = GetOpacityTransferFunction(colorVar)
+        # HideScalarBarIfNotNeeded(wLUT, renderView1)
+
+        # ## NOTE: For color presets.
+        # wLUT.ApplyPreset('Cool to Warm (Extended)', True)
+
+        # renderView1.Update()
+        # UpdateScalarBars()
+
+        # projectionDisplay.SetScalarBarVisibility(renderView1, scalarBarVisible)
+
+        # SaveScreenshot(colorVar + str(count)+'.png', renderView1, ImageResolution=[1750, 1300], TransparentBackground=0)
+
+        # Hide(projection, renderView1)
+        ## }}}
+
         integrated = IntegrateVariables(Input=projection)
-
         intdata = servermanager.Fetch(integrated)
         intdata = dsa.WrapDataObject(intdata)
-        value = intdata.PointData['scalar_3']
-        zarr.append(value)
+        value = intdata.PointData[colorVar]
+        try:
+            value = ns.vtk_to_numpy(value)
+            flowrate.append(value[0])
+            zs.append(zpos)
+        except:
+            pass
 
-    zarr=[ns.vtk_to_numpy(x)[0] for x in zarr[2:]]
-    print(zarr)
+    print(zs)
+    print(flowrate)
+    csvWriter('massFlux.csv', zs, flowrate)
     plt.figure()
-    plt.plot(zarr)
+    plt.plot(zs, flowrate)
     plt.savefig('plot.pdf')
     # plt.show()
 
-
 def snapshot(reader, **kwargs):
-    projectionType = kwargs.get('projectionType', 'clip')
-    colorVars = kwargs.get('colorVars', reader.PointArrayStatus) or reader.PointArrayStatus
     scalarBarVisible = kwargs.get('scalarBarVisible', True)
     axisVisible = kwargs.get('axisVisible', True)
     geometry = kwargs.get('geometry')
+    zoom = kwargs.get('zoom')
+
+    renderView1 = GetActiveViewOrCreate('RenderView')
+    display = Show(reader, renderView1)
+    # display.Representation = 'Surface'
+    display.Representation = 'Surface With Edges'
+    renderView1.OrientationAxesVisibility = int(axisVisible)
+    # display.RescaleTransferFunctionToDataRange()
+
+    renderView1.Update()
+    renderView1.ResetCamera()
+    renderView1.ViewSize = geometry
+    renderView1.CameraPosition = [0.0005945160428284565, 1.959300980464672e-13, -1.3552527156068805e-20]
+    renderView1.CameraFocalPoint = [-2.549999918319142e-05, 1.959300980464672e-13, -1.3552527156068805e-20]
+    renderView1.CameraViewUp = [0.0, 0.0, 1.0]
+    renderView1.CameraParallelScale = 0.00016047195994169908
+    renderView1.ResetCamera()
+
+    cam = GetActiveCamera()
+    cx,cy,cz = cam.GetPosition()
+    cam.SetPosition(cx/zoom[0], cy/zoom[0], cz/zoom[0])
+    Render()
+
+    timeKeeper1 = GetTimeKeeper()
+    for ts in reader.TimestepValues:
+        timeKeeper1.Time = ts
+        SaveScreenshot(str(ts)+'.png', renderView1, ImageResolution=[1750, 1300], TransparentBackground=1)
+
+def animate(reader, **kwargs):
+    projectionType = kwargs.get('projectionType', 'clip')
+    scalarBarVisible = kwargs.get('scalarBarVisible', True)
+    axisVisible = kwargs.get('axisVisible', True)
+    geometry = kwargs.get('geometry')
+    colorVars = kwargs.get('colorVars', reader.PointArrayStatus) or reader.PointArrayStatus
 
     animationScene1 = GetAnimationScene()
     timeKeeper1 = GetTimeKeeper()
@@ -120,7 +315,7 @@ def snapshot(reader, **kwargs):
     renderView1 = GetActiveViewOrCreate('RenderView')
     projectionDisplay = Show(projection, renderView1)
     projectionDisplay.Representation = 'Surface'
-    projectionDisplay.SetScalarBarVisibility(renderView1, scalarBarVisible)
+    # projectionDisplay.Representation = 'Surface With Edges'
     renderView1.OrientationAxesVisibility = int(axisVisible)
     projectionDisplay.RescaleTransferFunctionToDataRange()
 
@@ -137,7 +332,6 @@ def snapshot(reader, **kwargs):
     for colorVar in colorVars:
         print("Animating", colorVar )
 
-
         ColorBy(projectionDisplay, ('POINTS', colorVar))
         projectionDisplay.RescaleTransferFunctionToDataRange()
 
@@ -151,23 +345,23 @@ def snapshot(reader, **kwargs):
         renderView1.Update()
         UpdateScalarBars()
 
-        # renderView1.ViewSize = [1750, 1300]
-        # SaveAnimation(colorVar + '.png', renderView1, ImageResolution=[1750, 1300], TransparentBackground=1, SuffixFormat='.%04d')
-
+        projectionDisplay.SetScalarBarVisibility(renderView1, scalarBarVisible)
         SaveAnimation(colorVar + '.png', renderView1, ImageResolution=geometry, TransparentBackground=1, SuffixFormat='.%04d')
-
 
 def main():
 
     ap = argparse.ArgumentParser()
 
     ap.add_argument("-p", "--projectionType", required=False, default='clip', help="projection type: clip | slice")
-    ap.add_argument("-s", "--snapshot", required=False, action='store_true', help="run snapshotter")
+    ap.add_argument("-a", "--animate", required=False, action='store_true', help="run animator")
     ap.add_argument("-d", "--distribute", required=False, help="Apply d3 filter and save data")
-    ap.add_argument("-m", "--mass-defect", required=False, action='store_true', help="Integrate and find mass_defect curve")
+    ap.add_argument("-m", "--mass-flux", required=False, help="Find mass flux at n different slices along the z direction")
+    ap.add_argument("-s", "--snapshot", required=False, action='store_true', help="run snapshotter")
+    ap.add_argument("-b", "--bead-loading", required=False, action='store_true', help="Output bead loading data")
 
     ap.add_argument("-c", "--colorVars", required=False, nargs='*', help="color map variable")
     ap.add_argument("-g", "--geometry", required=False, nargs=2, type=int, default=[1750, 1300], help="Animation geometry size")
+    ap.add_argument("-z", "--zoom", required=False, nargs=3, type=float, default=[1, 1, 1], help="Zoom factors for snapshot")
     ap.add_argument("-nsb", "--no-scalar-bar", required=False, action='store_true', default=False, help="Disable scalar bar visibility")
     ap.add_argument("-nca", "--no-coordinate-axis", required=False, action='store_true', default=False, help="Disable coordinate axis visibility")
 
@@ -185,7 +379,11 @@ def main():
     if len(args['FILES']) == 0:
         filetype = args['filetype']
 
-        args['FILES'] = sorted([ file for file in os.listdir(".") if file.endswith(filetype) ], key=lambda x: int(x.split('.')[0].split('_')[-1]))
+        try:
+            args['FILES'] = sorted([ file for file in os.listdir(".") if file.endswith(filetype) ], key=lambda x: int(x.split('.')[0].split('_')[-1]))
+        except:
+            print("Not sorting files.")
+            args['FILES'] = [ file for file in os.listdir(".") if file.endswith(filetype) ]
 
         if len(args['FILES']) == 0:
             print("Didn't find", filetype, "files in current folder.")
@@ -208,7 +406,7 @@ def main():
     elif filetype == 'pvtu':
         reader = XMLPartitionedUnstructuredGridReader(FileName=args['FILES'])
     elif filetype == 'vtk':
-        reader = LegacyVTKReader(FileName=args['FILES'])
+        reader = LegacyVTKReader(FileNames=args['FILES'])
     else:
         print("Unsupported File Format!")
         sys.exit(-1)
@@ -219,8 +417,8 @@ def main():
                 Writealltimestepsasfileseries=1)
         sys.exit(0)
 
-    if args['snapshot']:
-        snapshot(reader,
+    if args['animate']:
+        animate(reader,
                 projectionType = args['projectionType'],
                 colorVars = args['colorVars'],
                 scalarBarVisible = not args['no_scalar_bar'],
@@ -228,8 +426,27 @@ def main():
                 axisVisible = not args['no_coordinate_axis']
                 )
 
-    if args['mass_defect']:
-        mass_defect(reader);
+    if args['snapshot']:
+        snapshot(reader,
+                scalarBarVisible = not args['no_scalar_bar'],
+                geometry = args['geometry'],
+                axisVisible = not args['no_coordinate_axis'],
+                zoom = args['zoom']
+                )
+
+    if args['mass_flux']:
+        mass_flux(reader,
+                scalarBarVisible = not args['no_scalar_bar'],
+                geometry = args['geometry'],
+                axisVisible = not args['no_coordinate_axis'],
+                colorVars = args['colorVars'],
+                nSlice = args['mass_flux']
+                )
+
+    if args['bead_loading']:
+        bead_loading(reader,
+                colorVars = args['colorVars']
+                )
 
     if args['writer']:
         writer=None
