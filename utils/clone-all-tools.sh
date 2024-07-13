8f2a0510bda14f69e49a05064b674a8276e5d6f2
Jayghosh Rao <jayghoshter@gmail.com>
2024-07-13 22:48:15 +0200

    update clone-all-tools to use github repos

--- utils/clone-all-tools.sh
+++ utils/clone-all-tools.sh
@@ -1,65 +1,21 @@
 #!/usr/bin/env bash
 
-# Repo with Andreas Puettmann's stuff:  svn+ssh://svn@ibtmodsimhub.ibt.kfa-juelich.de/puettmann
+TOOLS=(genmesh pymesh mixdtools paravision chromoo scripts)
+URL=git@github.com:modsim
 
-set -euo pipefail
-
-## NOTE: Usage: 
-# ./clone-all-tools.sh will clone everything listed below to the current directory
-#       --https: will pull via https
-#       --ssh: will pull via ssh, default
-#       any positional arguments left are considered repo names to be pulled
-
-BRANCH="master"
-ARGS=""
-MODE="SSH"
-RMGIT=false # flag to remove the .git folder. Useful in JURECA to not get prompted the password thanks to my shell trying to access git stats
-ALL=( checkperiodic extractRNG extractTS genmesh genmprd gennmat gmsh2mixd gmsh2mixdv2 mapflow mixd2pvtu mixdclass mixdsplit mixdvolume normmixd pymesh rmmat scalemixd scripts shiftmixd stitchperiodic xcad )
-
-REPOS=()
+POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
     key="$1"
-
     case $key in
         --https)
-            echo "Cloning with HTTPS"
-            MODE="HTTPS"
-            shift # past value
-            ;;
-        --ssh)
-            echo "Cloning with SSH"
-            MODE="SSH"
-            shift # past value
-            ;;
-        --no-git)
-            echo "Performing a shallow clone and removing git history"
-            ARGS="$ARGS --depth 1"
-            RMGIT=true
-            shift # past value
-            ;;
-        *)    # unknown option
-            REPOS+=("$1") # save it in an array for later
-            shift # past argument
-            ;;
+            URL=https://github.com/modsim; shift ;;
+        *)
+            POSITIONAL+=("$1"); shift ;;
     esac
 done
-set -- "${REPOS[@]}" # restore positional parameters
+set -- "${POSITIONAL[@]}" # restore positional parameters
 
-if [[ "$MODE" == "SSH" ]]; then 
-    SERVER="git@jugit.fz-juelich.de:IBG-1/ModSim/Chroma-HD"
-else
-    SERVER="https://jugit.fz-juelich.de/IBG-1/ModSim/Chroma-HD"
-fi
-
-[[ ${#REPOS[@]} == 0 ]] && REPOS=${ALL[@]}
-
-for REPO in ${REPOS[@]}; do 
-    git clone $ARGS "$SERVER/$REPO.git"
-    if [ $RMGIT = true ]; then
-        rm -rf "$REPO/.git"
-    fi
+for TOOL in "${TOOLS[@]}"; do
+    git clone "$URL/ChromaHD-$TOOL" "$TOOL"
 done
-
-# https://jugit.fz-juelich.de/IBG-1/ModSim/Chroma-HD/$REPO/-/archive/master/$REPO-master.tar.gz
-#
